865419ac582a73c1846cfbd3b9de124d9cc0d53c
ali.abukahil <ali.abukahil@gmail.com>
2021-05-05 14:02:23 +0200

    repeating some lessons

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,31 @@
 // Variables
-const button = document.querySelector("#btn");
+const year = new Date().getFullYear();
+document.querySelector(".year").innerHTML = year;
+
+const date = new Date();
+const checkNow = document.querySelector("#check-now");
+document.querySelector("#title").innerHTML = "Days Counter";
+
+const getDayName = () => {
+  return date.toLocaleDateString("en-EN", {
+    weekday: "long",
+  });
+};
 
 // Event listeners
 
-button.addEventListener("click", function () {
-  let money = document.querySelector("#money").value;
-  let numUSD = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  });
-  document.querySelector("#output").innerHTML =
-    "The newly formatted currency number is" + numUSD.format(money);
-});
+//Functions
+
+function weekend(e) {
+  e.preventDefault();
+  const userName = document.querySelector("#user-name").value;
+  const result = document.querySelector(".result");
+  if (userName != "") {
+    result.innerHTML = `What'up! ${userName}, Today is ${getDayName()}, ${
+      5 - date.getDay()
+    } days let till weekend`;
+  } else {
+    document.querySelector("#user-name").placeholder = "Do it or not up to U";
+  }
+}
+checkNow.addEventListener("click", weekend);
